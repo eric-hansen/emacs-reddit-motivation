a9704506bc9f9656a0925bab33aa196d59392686
Eric Hansen <eric.hansen@ninjageekstudios.com>
2016-03-31 20:18:49 -0400

    Made it so the syntax is correct

--- reddit-motivation.el
+++ reddit-motivation.el
@@ -4,14 +4,35 @@
   "Fetches a (random) Imgur link from /r/GetMotivated and load it in the browser"
   (interactive)
   (request
-    "https://www.reddit.com/r/GetMotivated/search.json?q=site%3Ai.imgur.com&restrict_sr=on&sort=relevance&t=link"
+    "http://www.reddit.com/r/GetMotivated/search.json?q=site%3Ai.imgur.com&restrict_sr=on&sort=relevance&t=link"
     :parser 'json-read
     :success (function*
         (lambda (&key data &allow-other-keys)
           ;; data -> children -> <random> -> data -> url
-          (let imgurl (cdr (assoc 'url (cdr (assoc 'data (cdr (assoc 0 (cdr (assoc 'children (cdr (assoc 'data data)))))))))))
+          (let imgurl 
+		(cdr 
+			(assoc 'url 
+				(cdr 
+					(assoc 'data 
+						(cdr 
+							(assoc 0 
+								(cdr 
+									(assoc 'children 
+										(cdr 
+											(assoc 'data data)
+										)
+									)
+								)
+							)
+						)
+					)
+				)
+			)
+		)
+	)
           (message "IMGUR URL: %s" imgurl)
         )
+    )
   ))
 
 (provide 'reddit-motivation)
